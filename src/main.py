3c53214e14012d7748296318b58704d21d5be007
RafaelEmery <rafael.emerycade@gmail.com>
2024-04-17 08:35:13 -0300

    chore: using env vars to run

--- src/main.py
+++ src/main.py
@@ -24,4 +24,6 @@ async def ok():
 if __name__ == '__main__':
 	import uvicorn
 
-	uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, log_level='info')
+	uvicorn.run(
+		'main:app', host=settings.APP_HOST, port=settings.APP_PORT, reload=True, log_level='info'
+	)
