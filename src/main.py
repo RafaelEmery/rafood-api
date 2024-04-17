d344997757ee064b2d7b96fb6c9e3c568b160c0a
RafaelEmery <rafael.emerycade@gmail.com>
2024-04-17 08:17:38 -0300

    style: adding ruff pre-commit

--- src/main.py
+++ src/main.py
@@ -6,22 +6,22 @@ from config import settings
 app = FastAPI(
     title=settings.APP_NAME,
     description=settings.APP_DESCRIPTION,
-    version=settings.APP_VERSION
+    version=settings.APP_VERSION,
 )
 
 
 @app.get(
-    '/ok',
-    tags=['Standard'],
+    "/ok",
+    tags=["Standard"],
     status_code=200,
-    name='Health Check Endpoint',
-    description='Check if the API is running.'
+    name="Health Check Endpoint",
+    description="Check if the API is running.",
 )
 async def ok():
-    return {'message': 'OK!'}
+    return {"message": "OK!"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, log_level='info')
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
