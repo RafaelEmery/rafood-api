b921317eef5e2d261598d5266c85d76d6b75063a
RafaelEmery <rafael.emerycade@gmail.com>
2026-01-18 18:22:03 -0300

    test: adding task for users endpoints

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,5 +1,6 @@
 from faker import Faker
 from tasks.categories_task_set import CategoryTasks
+from tasks.users_task_set import UserTasks
 
 from locust import HttpUser, between
 
@@ -7,6 +8,6 @@ faker = Faker()
 
 
 class RafoodAPILoadTest(HttpUser):
-	tasks = [CategoryTasks]
+	tasks = [CategoryTasks, UserTasks]
 	host = 'http://localhost:8000'
 	wait_time = between(1, 3)
---
+++ locust/tasks/users_task_set.py
@@ -0,0 +1,96 @@
+import json
+
+from faker import Faker
+
+from locust import TaskSet, task
+
+faker = Faker()
+
+
+def log_failure(action: str, response, extra_info=None):
+	print(f'[{action}] - Status: {response.status_code}')
+	print(f'Response: {response.text}')
+	if extra_info:
+		print(f'Info: {json.dumps(extra_info, indent=2, default=str)}')
+
+
+class UserTasks(TaskSet):
+	def create_user(self):
+		user_data = {
+			'first_name': faker.first_name(),
+			'last_name': faker.last_name(),
+			'email': faker.email(),
+			'password': faker.password(length=12),
+		}
+
+		response = self.client.post('/api/v1/users', json=user_data, name='/api/v1/users [CREATE]')
+
+		if response.status_code != 201:
+			log_failure('Create User', response, user_data)
+			return None
+
+		return response.json().get('id')
+
+	def get_users(self):
+		response = self.client.get('/api/v1/users', name='/api/v1/users [LIST]')
+
+		if response.status_code != 200:
+			log_failure('Get Users', response)
+			return []
+
+		users = response.json()
+		return [user.get('id') for user in users if user.get('id')]
+
+	def get_user_details(self, user_id: str):
+		response = self.client.get(f'/api/v1/users/{user_id}', name='/api/v1/users/{id} [GET]')
+
+		if response.status_code != 200:
+			log_failure('Get User Details', response, {'user_id': user_id})
+			return None
+
+		return response.json().get('id')
+
+	def update_user(self, user_id: str):
+		update_data = {
+			'first_name': faker.first_name(),
+			'last_name': faker.last_name(),
+		}
+
+		response = self.client.put(
+			f'/api/v1/users/{user_id}', json=update_data, name='/api/v1/users/{id} [UPDATE]'
+		)
+
+		if response.status_code != 200:
+			log_failure('Update User', response, {'user_id': user_id, 'data': update_data})
+			return None
+
+		return response.json().get('id')
+
+	def delete_user(self, user_id: str):
+		response = self.client.delete(
+			f'/api/v1/users/{user_id}', name='/api/v1/users/{id} [DELETE]'
+		)
+
+		if response.status_code != 204:
+			log_failure('Delete User', response, {'user_id': user_id})
+
+	@task
+	def user_lifecycle(self):
+		"""Full lifecycle: create, list, get details, update, delete"""
+		user_id = self.create_user()
+		if not user_id:
+			return
+
+		users = self.get_users()
+		if not users:
+			return
+
+		user_id = self.get_user_details(user_id)
+		if not user_id:
+			return
+
+		user_id = self.update_user(user_id)
+		if not user_id:
+			return
+
+		self.delete_user(user_id)
